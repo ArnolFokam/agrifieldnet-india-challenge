d7853ad4e89f4e2b95c3bdb4359aabced19678d2
Arnol Fokam <arnolfokam23@gmail.com>
2022-10-07 14:17:51 +0200

    adfd ability to preprocess downloaded dataset

--- aic/dataset.py
+++ aic/dataset.py
@@ -4,12 +4,14 @@ import getpass
 from typing import List
 
 import torch
+import rasterio
 import numpy as np
+from tqdm import tqdm
 from dotenv import load_dotenv
 from radiant_mlhub import Dataset
 from typing import Callable, Optional
 
-from aic.helpers import get_dir
+from aic.helpers import get_dir, get_folder_ids
 
 
 load_dotenv()
@@ -26,7 +28,7 @@ class AgriFieldDataset(torch.utils.data.Dataset):
         train: bool = True,
         download: bool = False,
         bands: Optional[List[str]] = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B11', 'B12'],
-        transform: Optional[Callable] =None):
+        transform: Optional[Callable] = None):
 
         self.bands = bands
         self.train = train
@@ -40,12 +42,74 @@ class AgriFieldDataset(torch.utils.data.Dataset):
         if download:
             self.download_data(root_dir, dataset_name, bands)
 
+        folder_ids = get_folder_ids(self.root_dir, dataset_name, label_collection)
+
         self.imgs: List[np.ndarray] = []
+        self.field_ids: List[str] = []
+        # self.field_id_to_imgs: List[str, int] = {}
         self.field_masks: List[np.ndarray] = []
         self.targets: List[int] = []
 
-    def __getitem__(self, index):
+        pbar = tqdm(folder_ids)
+        pbar.set_description('Extracting Images, Field Masks and Target Variable')
+
+        for fidx in pbar:
+
+            with rasterio.open(f'{self.root_dir}/{dataset_name}/{label_collection}/{label_collection}_{fidx}/field_ids.tif') as src:
+                field_data = src.read()[0]
+
+            # get bands for folder id
+            bands_src = [rasterio.open(f'{self.root_dir}/{dataset_name}/{source_collection}/{source_collection}_{fidx}/{band}.tif') for band in self.bands]
+            bands_array = [np.expand_dims(band.read(1), axis=0) for band in bands_src]
+            bands = np.vstack(bands_array)
+            
+
+            if self.train:
+                with rasterio.open(f'{self.root_dir}/{dataset_name}/{label_collection}/{label_collection}_{fidx}/raster_labels.tif') as src:
+                    label_data = src.read()[0]
+
+            # get unique field ids in the field data
+            field_ids = list(np.unique(field_data))
+            field_ids.remove(0)
+
+            for fid in field_ids:
+
+                # append field ids
+                self.field_ids.append(fid)
+
+                # some chips have more that one training field, so, 
+                # it is better to have a list of unique chips 
+                # and a reference to the correct chip per field 
+                # id rather than having the same chip for every field id
+                # more space economical
+                #
+                # NOTE: we can't build a mapping because the field ids 
+                # and the bands are in a many-to-many relationship
+                #
+                # last_img_index = len(self.imgs) - 1 # index of the current spectral bands
+                # self.field_id_to_imgs[fid] = last_img_index
+                
+                # append spectral bands
+                self.imgs.append(bands)
+
+                # append field mask
+                mask = np.where(field_data == fid, 1.0, 0.0)
+                self.field_masks.append(mask)
+
+                if self.train:
+
+                    # get pixels with crop label per band
+                    label = int(label_data[field_data == fid].mean())
+
+                    assert (label_data[field_data == fid] == label).all(), "[Corrupted Data] field data has more than one label."
+
+                    # append label
+                    self.targets.append(label)
+
+
+    def __getitem__(self, index: str):
         pass
+
 
     def download_data(
         self, 
@@ -74,5 +138,7 @@ class AgriFieldDataset(torch.utils.data.Dataset):
             collection_filter=filters
         )
 
+    
+
 if __name__ == '__main__':
-    ds = AgriFieldDataset('data/source', download=True)
+    ds = AgriFieldDataset('data/source', train=True)
--- aic/helpers.py
+++ aic/helpers.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 def get_dir(*paths) -> str:
     """Creates a dir from a list of directories (like os.path.join), runs os.makedirs and returns the name
@@ -8,3 +9,18 @@ def get_dir(*paths) -> str:
     directory = os.path.join(*paths)
     os.makedirs(directory, exist_ok=True)
     return directory
+
+def get_folder_ids(data_dir, dataset_name, collection):
+    """
+    Get folder id for each chip in the dataset in either train or test test
+
+    Args:
+        data_dir: data directory where the folders for each chip are found
+        dataset_name: name of the dataset to use
+        collection: collection to use
+    """
+    with open (f'{data_dir}/{dataset_name}/{collection}/collection.json') as f:
+        collention_json = json.load(f)
+        collention_folder_ids = [i['href'].split('_')[-1].split('.')[0] for i in collention_json['links'][4:]]
+
+        return collention_folder_ids
