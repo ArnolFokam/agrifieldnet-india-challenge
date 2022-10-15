75960cb406add689655e3bf8c69ab5d36a1a73ad
Arnol Fokam <arnolfokam23@gmail.com>
2022-10-15 07:55:27 +0200

    modify train script

--- train_model.sh
+++ train_model.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 python -m scripts.run_exp train \
-    --download_data=True \
     --partition_name='batch' \
-    --use_slurm=True
-#    --sweep_path=exps/sweep.yaml \
-#    --sweep_count=50 \
+    --use_slurm=True \
+    --sweep_path=exps/sweep.yaml \
+    --sweep_count=50
